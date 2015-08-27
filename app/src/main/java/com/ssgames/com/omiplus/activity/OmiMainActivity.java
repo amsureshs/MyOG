e17dd44d6bc39e505405f3992d21e1a149bf91bc
amsureshs <sureshsumanasooriya@gmail.com>
2015-08-27 14:32:14 +0530

    first commit

---
+++ app/src/main/java/com/ssgames/com/omiplus/activity/OmiMainActivity.java
@@ -0,0 +1,25 @@
+package com.ssgames.com.omiplus.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ssgames.com.omiplus.R;
+
+public class OmiMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_omi_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
